c49fdc368ff356b5ef1052a078c4d09e0a428864
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-03 01:32:04 +0500

    complt

---
+++ main.js
@@ -0,0 +1,4 @@
+// 2.	Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var person_name = "Muhammad ASADIFTEKHAR";
+console.log("\"Hello ".concat(person_name, ", would you like to learn some TypeScript today?\""));
